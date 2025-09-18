f3db35d78113bdd75b33ce6ec2983bd4f0da3456
pushpendra <pushpendrathakur145@gmail.com>
2025-09-18 17:16:59 +0530

    added remaining file

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,117 @@
+import MoiveCard from '@/components/MoiveCard'
+import SearchBar from '@/components/SearchBar'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api'
+import { updateSearchCount } from '@/services/appwrite'
+import useFetch from '@/services/useFetch'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
+
+const search = () => {
+  const [searchQuery,setSearchQuery]=useState('');
+   const {
+    data:movies,
+    loading:moviesLoading,
+     error:moviesErrors,
+     refetch:loadMovies,
+     reset
+    } = 
+     useFetch(()=>
+      fetchMovies(
+      {
+        query:searchQuery
+      }
+    ),false);
+    useEffect( ()=>{
+      
+      const timeoutId = setTimeout( async()=>{
+      if(searchQuery.trim()){
+        await loadMovies();
+        if(movies?.length>0 && movies?.[0] )
+          console.log('fdrgtg');
+          console.log('First movie from search',movies?.[0]);
+        // await updateSearchCount(searchQuery,movies?.[0]);
+      }
+      else{
+        reset();
+      }
+    },500);
+   
+    return ()=>clearTimeout(timeoutId);
+    },[searchQuery])
+    useEffect(() => {
+    if (movies?.length > 0) {
+      // Call updateSearchCount with query + first movie
+      updateSearchCount(searchQuery, movies[0])
+        .catch(err => console.error("updateSearchCount error:", err));
+    }
+  }, [movies]); // 👈 runs only when movies updates
+
+  return (
+    <View className='flex-1 bg-primary '>
+      <Image source={images.bg} className='flex-1 absolute w-full z-0'  resizeMode='cover'/>
+         
+         <FlatList 
+           data={movies}
+           renderItem={({item})=>(
+            <MoiveCard {...item} />
+           )}
+           keyExtractor={(item)=>item.id.toString()} 
+           className='px-5'
+           numColumns={3}
+            columnWrapperStyle={{justifyContent:'center', 
+              gap:16,
+               marginVertical:16
+            }}
+            contentContainerStyle={{paddingBottom:100 }}
+            ListHeaderComponent={
+              <>
+              <View className='w-full flex-row justify-center mt-20 items-center'>
+                <Image source={icons.logo} className='w-12 h-10' />
+              
+           </View>
+           <View className='my-5'>
+            <SearchBar
+                value={searchQuery}
+                onChangeText={(text:string)=>setSearchQuery(text)}
+                placeholder="Search for a movie..."
+            />
+           </View>
+           {moviesLoading && (
+            <ActivityIndicator size="large" color="#0000ff" className='my-3'  />
+           )}
+           {
+            moviesErrors && (
+               <Text className='text-red-500 px-5 my-3'>
+                Error: {moviesErrors?.message}
+               </Text>
+            )
+           }
+           {
+            !moviesLoading && !moviesErrors && searchQuery.trim() && movies?.length>0 && (
+              <Text className='text-xl text-white font-bold '>
+                Search Results For {' '}
+                <Text className='text-accent'>{searchQuery}</Text>
+              </Text>
+            ) 
+           }
+              </>
+            }
+            ListEmptyComponent={
+              !moviesLoading && !moviesErrors ? (
+                <View className='mt-10 px-5 '>
+                  <Text className='text-center text-gray-500' >
+                    {searchQuery.trim() ? 'No movies found' : 'Start typing to search for movies'}
+                  </Text>
+
+                 </View> 
+              ):null
+            }
+          />
+
+    </View>
+  )
+}
+
+export default search
